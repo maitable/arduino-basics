123924d231b077d1e0b43c75fded7b2315d31fdd
maita <maiapricop18@gmail.com>
2026-06-17 15:33:15 +0000

    organize

--- motorfataspate/motorfataspate.ino
+++ motorfataspate/motorfataspate.ino
@@ -1,9 +1,9 @@
-const int M1A = 9;  // define pin 12 for A-1A 
-const int M1B = 10;  // define pin 14 for A-1B 
-const int M2A = 3;  // define pin 12 for A-1A 
-const int M2B = 5;  // define pin 14 for A-1B 
-int speedM1=200;
-int speedM2=200;
+const int M1A = 18;  // define pin 12 for A-1A 
+const int M1B = 19;  // define pin 14 for A-1B 
+const int M2A = 16;  // define pin 12 for A-1A 
+const int M2B = 17;  // define pin 14 for A-1B 
+int speedM1=2;
+int speedM2=255;
 
 void setup() {
   pinMode(M1A, OUTPUT);     // specify these pins as outputs
@@ -17,54 +17,37 @@ void setup() {
 }
 void loop() 
 {
-spate();
-delay(2000);
 fata();
-delay(2000);
-stanga();
-delay(2000);
-dreapta();
 delay(2000);
 
 }
 void fata() {
- for(int i=180;i<255;i=i+10)
-{ speedM1=i;
- speedM2=i;
   digitalWrite(M1A, speedM1);   
   digitalWrite(M1B, LOW);
 	digitalWrite(M2A, speedM2);   
-  digitalWrite(M2B, LOW);}
+  digitalWrite(M2B, LOW);
 
 }
 void spate() {
-  for(int i=180;i<255;i=i+10){
- speedM1=i;
- speedM2=i;
   digitalWrite(M1A, LOW);   
   digitalWrite(M1B, speedM1);
 	digitalWrite(M2A, LOW);   
   digitalWrite(M2B, speedM2);
-  }
+
 }
 void stanga() {
-  for( int i=180;i<255;i=i+10){
- speedM1=i;
- speedM2=180;
 digitalWrite(M1A, LOW);   
 digitalWrite(M1B, speedM1);
 digitalWrite(M2A, LOW);   
-digitalWrite(M2B, speedM2);}
+digitalWrite(M2B, speedM2);
 }
 void dreapta() {
- for(int  i=180;i<255;i=i+10){
- speedM2=i;
- speedM1=180;
+
 digitalWrite(M1A, speedM1);   
 digitalWrite(M1B, LOW);
 digitalWrite(M2A, speedM2);   
 digitalWrite(M2B, LOW);
-}}
+}
 void stop() 
 { digitalWrite(M1A, LOW);   
 digitalWrite(M1B, LOW);
